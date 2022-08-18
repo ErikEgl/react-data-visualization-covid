9df75a1c3b2c81dee7c3962c659fbc521eac6f8f
Erik <Mr.erik.egliens@gmail.com>
2022-08-19 00:13:14 +0300

    added ChartBlock to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import ModeToggle from "./components/ModeToggle/ModeToggle";
+import ChartBlock from "./components/ChartBlock/ChartBlock";
+
 import { UserContext } from "./utils/useContext";
+
+
 function App() {
   const { isDarkMode } = useContext(UserContext);
   return (
     <main className={isDarkMode ? 'dark-mode' : 'light-mode'}>
       <ModeToggle />
+      <ChartBlock />
     </main>
   );
 }
