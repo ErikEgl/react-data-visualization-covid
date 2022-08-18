df15135e9bae86ac6a5d3fbf37e44fc04040ff01
Erik <Mr.erik.egliens@gmail.com>
2022-08-19 00:10:45 +0300

    added bootstrap css styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AppContextProvider } from "./utils/useContext";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <AppContextProvider>
