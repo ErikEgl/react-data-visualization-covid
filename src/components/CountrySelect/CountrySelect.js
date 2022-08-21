b5ded3b7aa2df7e1045b366e757fb54106bebd0f
Erik <Mr.erik.egliens@gmail.com>
2022-08-22 00:04:55 +0300

    select form for coutry select

--- src/components/CountrySelect/CountrySelect.js
+++ src/components/CountrySelect/CountrySelect.js
@@ -1,8 +1,34 @@
-import React from "react";
-
+import { useContext } from "react";
+import { UserContext } from "../../utils/useContext";
+import CountryOptionItem from '../CountryOptionItem/CountryOptionItem'
 function CountrySelect() {
+  const { selectCountry, selectedCountry, covidData } = useContext(UserContext);
+
+
+  let keys = [];
+  for (let key in covidData) {
+      keys.push(key); // pushes each key into keys arrays
+  }
+  const items = keys.map(key => <CountryOptionItem key={key} location={covidData[key].location}/>)
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    selectCountry({[name]: value})
+}
   return (
-    <>CountrySelect</>
+    <>
+        <form>
+          <select 
+              id="countrySelect"
+              value={selectedCountry.length ? selectedCountry : ""}
+              onChange={handleChange}
+              name="countrySelect"
+          >
+              <option value="">-- Choose --</option>
+              {items}
+          </select>
+        </form>
+    </>
   );
 }
 export default CountrySelect;
