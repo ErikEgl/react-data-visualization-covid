86f785d92247d54a26348f770be140f41dd908c8
Erik <Mr.erik.egliens@gmail.com>
2022-08-24 18:54:48 +0300

    removed custom dark mode code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { useContext } from "react";
 import ModeToggle from "./components/ModeToggle/ModeToggle";
 import ChartBlock from "./components/ChartBlock/ChartBlock";
 
-import { UserContext } from "./utils/useContext";
-
 
 function App() {
-  const { isDarkMode } = useContext(UserContext);
   return (
-    <main className={isDarkMode ? 'dark-mode' : 'light-mode'}>
-      <ModeToggle />
-      <ChartBlock />
-    </main>
+    <>
+      <main>
+        <ModeToggle />
+        <ChartBlock />
+      </main>
+    </>
   );
 }
 export default App;
