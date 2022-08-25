c6ca510a79d98b746dfd7ad9dc96ab900ed85144
Erik <Mr.erik.egliens@gmail.com>
2022-08-26 00:41:21 +0300

    radio button styles

--- src/components/ChartBlock/ChartBlock.js
+++ src/components/ChartBlock/ChartBlock.js
@@ -1,19 +1,18 @@
 import React from "react";
-import CountrySelect from "../CountrySelect/CountrySelect"
+import CountrySelect from "../CountrySelect/CountrySelect";
 import ChartControls from "../ChartControls/ChartControls";
-import { Tab, Tabs } from 'react-bootstrap';
-import { BarChart, LineChart, LineSeries, PointSeries } from 'reaviz';
+import { Tab, Tabs } from "react-bootstrap";
+import { BarChart, LineChart, LineSeries, PointSeries } from "reaviz";
 import { useContext } from "react";
 import { UserContext } from "../../utils/useContext";
 
-
 function ChartBlock(props) {
-  const { selectedCountry, selectedOptions} = useContext(UserContext);
+  const { selectedCountry, selectedOptions } = useContext(UserContext);
   const { covidData } = useContext(UserContext);
   const chartControlNames = {
     name1: "ReportedCases",
-    name2: "RankedCharts"
-  }
+    name2: "RankedCharts",
+  };
   return (
     <div className="chart-block">
       <CountrySelect />
@@ -23,28 +22,48 @@ function ChartBlock(props) {
         className="mb-3"
       >
         <Tab eventKey={chartControlNames.name1} title={chartControlNames.name1}>
-        {selectedCountry && selectedCountry.countrySelect }
-        <br/>
-        <h1>
-          Line chart {chartControlNames.name1 === selectedOptions.ReportedCases.name && selectedOptions.ReportedCases.value}
-        </h1>
-        <br/>
-        <ChartControls name={chartControlNames.name1} control="Death count"/>
-        <ChartControls name={chartControlNames.name1} control="Confirmed cases"/>
+          {selectedCountry && selectedCountry.countrySelect}
+          <br />
+          <h1>
+            Line chart{" "}
+            {chartControlNames.name1 === selectedOptions.ReportedCases.name &&
+              selectedOptions.ReportedCases.value}
+            <br />
+            Line chart{" "}
+            {chartControlNames.name2 === selectedOptions.RankedCharts.name &&
+              selectedOptions.RankedCharts.value}
+          </h1>
+          <br />
+          <div data-theme="dark" className="d-flex gap-2">
+            <div className="d-flex rounded border border-5 justify-content-center align-items-center">
+              <ChartControls
+                name={chartControlNames.name1}
+                control="Death count"
+              />
+              <ChartControls
+                name={chartControlNames.name1}
+                control="Confirmed cases"
+              />
+            </div>
+            <div className="d-flex rounded border border-5 justify-content-center align-items-center">
+              <ChartControls
+                name={chartControlNames.name2}
+                control="Daily new values"
+              />
+              <ChartControls
+                name={chartControlNames.name2}
+                control="Cumulative mode"
+              />
+            </div>
+          </div>
         </Tab>
         <Tab eventKey={chartControlNames.name2} title={chartControlNames.name2}>
-        {selectedCountry && selectedCountry.countrySelect }
-        <br/>
-        <h1>
-          Bar chart {chartControlNames.name2 === selectedOptions.RankedCharts.name && selectedOptions.RankedCharts.value}
-        </h1>
-        <br/>
-        <ChartControls name={chartControlNames.name2} control="Daily new values"/>
-        <ChartControls name={chartControlNames.name2} control="Cumulative mode"/>
+          {selectedCountry && selectedCountry.countrySelect}
+          <br />
+          <h1>Bar chart</h1>
+          <br />
         </Tab>
       </Tabs>
-      
-      
     </div>
   );
 }
