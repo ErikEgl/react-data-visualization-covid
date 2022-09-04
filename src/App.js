b87f92952fc85891178b33935cf3700e467b1ecb
Erik <Mr.erik.egliens@gmail.com>
2022-09-04 21:06:06 +0300

    added loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "./utils/useContext";
+
 import ModeToggle from "./components/ModeToggle/ModeToggle";
 import ChartBlock from "./components/ChartBlock/ChartBlock";
+import Spinner from 'react-bootstrap/Spinner';
 
 
 function App() {
+  const { isLoading } = useContext(UserContext);
+  console.log(isLoading);
   return (
     <>
-      <main>
+      <main>    
+        {isLoading && <Spinner animation="border" role="status"></Spinner>}
         <ModeToggle />
         <ChartBlock />
       </main>
